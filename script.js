bf3666588560e28ccce701c90765ff8f60275fac
orcely <76852831+orcynt@users.noreply.github.com>
2024-12-14 00:42:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
+function closeMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'none';
+}
